5e39a2ad7314b5911bf33b91d14bfe880180dded
maria-raquel <mariaraquelmartinez97@gmail.com>
2022-05-28 20:16:16 -0300

    criacao do arquivo matrix.c

---
+++ matrix.c
@@ -0,0 +1,24 @@
+/*
+>>>>>>>>>>> PROVA_2_PROGRAMACAO_ESTRUTURADA <<<<<<<<<<<<<
+
+* Professor: Igor Malheiros
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>|<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+Alunos:
+- Joao Antonio Honorato | 20210026680
+- Maria Raquel Martinez | 20200025900
+*/
+
+#include "matrix.h"
+
+//>================ Implementacoes de Funcoes: =================
+
+//>==================== criacao de matrizes: ===================
+
+//>==================== acessar elementos: =====================
+
+//>================= manipulacao de dimensoes: =================
+
+//>======================== agregacao: =========================
+
+//>================== operacoes aritmeticas: ===================
