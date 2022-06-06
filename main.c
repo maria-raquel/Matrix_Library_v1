71f9f223bd422ec1fd79c05481f5a563e254af08
maria-raquel <mariaraquelmartinez97@gmail.com>
2022-06-06 10:01:32 -0300

    organização dos testes

--- main.c
+++ main.c
@@ -1,61 +1,99 @@
 /*
->>>>>>>>>>> PROVA_2_PROGRAMACAO_ESTRUTURADA <<<<<<<<<<<<<
+>>>>>>>>>>> PROJETO FINAL DE PROGRAMACAO ESTRUTURADA <<<<<<<<<<<<<
 
-* Professor: Igor Malheiros
+Professor: 
+Igor Malheiros
 
->>>>>>>>>>>>>>>>>>>>>>>>>>|<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 Alunos:
-- Joao Antonio Honorato | 20210026680
-- Maria Raquel Martinez | 20200025900
-*/
+Joao Antonio Honorato | 20210026680
+Maria Raquel Martinez | 20200025900
 
+*/
 
 #include "matrix.h"
 #include <stdio.h>
 
 int main(){
-    int array[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    int array2[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+    int array[] = {2, 13, 6, 1, 17, 4, 8, 14, 6, 11, 14, 4};
+    Matrix teste, op_1, op_2;
+    teste = create_matrix(array, 3, 4);
+    op_1 = create_matrix(array, 2, 2);
+    op_2 = full_matrix(2, 2, 2);
 
-    // print_matrix(create_matrix(array, 1, 9));
-    // putchar('\n');
-    // print_matrix(create_matrix(array2, 3, 4));
-    // putchar('\n');
-    // print_matrix(zeros_matrix(1,4));
-    // putchar('\n');
-    // print_matrix(full_matrix(3,3,13));
-    // putchar('\n');
-    // print_matrix(i_matrix(2));
-    // putchar('\n');
-    // print_matrix(tile_matrix(i_matrix(2), 5));
+    puts("//>==================== criacao de matrizes: ===================");
+    puts("Create matrix:");
+    print_matrix(teste);
+    putchar('\n');
 
+    puts("Zeros matrix:");
+    print_matrix(zeros_matrix(2, 4));
+    putchar('\n');
 
+    puts("Full matrix:");
+    print_matrix(full_matrix(5, 2, 13));
+    putchar('\n');
 
-    // Matrix mcreate = create_matrix(array2, 3, 4);
-    // put_element(mcreate, 2, 8, 44);
-    // print_matrix(mcreate);
-    // putchar('\n');
+    puts("I matrix:");
+    print_matrix(i_matrix(4));
+    putchar('\n');
 
-    // printf("\n%d", get_element(create_matrix(array2, 3, 4), 2, 8));
-    // putchar('\n');
+    puts("Tile matrix:");
+    print_matrix(tile_matrix(i_matrix(3), 3));
+    putchar('\n');
 
-    // putchar('\n');
-    // print_matrix(transpose(i_matrix(3)));
-
-    // print_matrix(reshape(create_matrix(array2, 3, 4), 2, 6));
-
-    // printf("\n");
-    // print_matrix(slice(create_matrix(array2, 3, 4), 1, 2, 3, 4));
-
-    // printf("%d\n", argmax(create_matrix(array2, 3, 4)));
-
+    puts("//>==================== acesso a elementos: ====================");
+    puts("Matrix a ser usada nos testes: ");
+    print_matrix(teste);
+    putchar('\n');
+    printf("Get element (1, 2): %d\n\n", get_element(teste, 1, 2));
     
+    put_element(teste, 0, 1, 0);
+    puts("Put element 0 em (0, 1): ");
+    print_matrix(teste);
+    putchar('\n');
 
-    print_matrix(division(full_matrix(3,3,5), full_matrix(3,3,0)));
+    puts("//>================= manipulacao de dimensoes: =================");
+    puts("Transpose: ");
+    print_matrix(transpose(teste));
+    putchar('\n');
 
+    puts("Reshape: ");
+    print_matrix(reshape(teste, 2, 6));
+    putchar('\n');
 
+    puts("Slice das linhas 1 a 2 e colunas 1 a 2:");
+    print_matrix(slice(teste, 1, 3, 1, 3));
+    putchar('\n');
 
+    puts("//>======================== agregacao: =========================");
+    printf("Min: %d\n\n", min(teste));
+    printf("Max: %d\n\n", max(teste));
+    printf("Argmin: %d\n\n", argmin(teste));
+    printf("Argmax: %d\n\n", argmax(teste));
 
+    puts("//>================== operacoes aritmeticas: ===================");
+    puts("Operando 1:");
+    print_matrix(op_1);
+    putchar('\n');
+    puts("Operando 2:");
+    print_matrix(op_2);
+    putchar('\n');
+
+    puts("Add: ");
+    print_matrix(add(op_1, op_2));
+    putchar('\n');
+
+    puts("Sub: ");
+    print_matrix(sub(op_1, op_2));
+    putchar('\n');
+
+    puts("Division: ");
+    print_matrix(division(op_1, op_2));
+    putchar('\n');
+
+    puts("Mul: ");
+    print_matrix(mul(op_1, op_2));
+    putchar('\n');
 
     return 0;
 }
